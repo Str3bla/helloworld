0de94a3a1ce542ba326596dc5443e7904ac0ff8b
Str3bla <reportingology@gmail.com>
2025-05-14 14:09:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,15 @@
 import streamlit as st
-from datetime import time, datetime
 
-st.header('st.slider')
-
-# Example 1
-
-st.subheader('Slider')
-
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
-
-# Example 2
-
-st.subheader('Range slider')
-
-values = st.slider(
-     'Select a range of values',
-     0, 100.0, (-12, 33))
-st.write('Values:', values)
-
-# Example 3
-
-st.subheader('Range time slider')
-
-appointment = st.slider(
-     "Schedule your appointment:",
-     value=(time(11, 33), time(12, 42)))
-st.write("You're scheduled for:", appointment)
-
-# Example 4
-
-st.subheader('Datetime slider')
-
-start_time = st.slider(
-     "When do you start?",
-     value=datetime(2020, 1, 1, 9, 30),
-     format="MM/DD/YY - hh:mm")
-st.write("Start time:", start_time)
+color = st.select_slider(
+    "Select a color of the rainbow",
+    options=[
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+    ],
+)
+st.write("My favorite color is", color)
