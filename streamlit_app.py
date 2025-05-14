f64e80a71389970832be2115804e6dc22ce42f58
Str3bla <reportingology@gmail.com>
2025-05-14 15:50:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ job_step = st.select_slider(
     ],
 )
 
-st.header('Segmented Trending by ', job_step)
+st.header("Segmented Trending by ", job_step)
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
