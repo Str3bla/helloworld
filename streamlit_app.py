9a21a716b6d291c052355f4d6b14e4827ffeb36f
Str3bla <reportingology@gmail.com>
2025-05-14 18:13:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,19 +14,19 @@ job_step = st.select_slider(
     ],
 )
 
-st.write(" ")
+st.markdown("<br>")
 
 option = st.selectbox(
     'What do you want to compare it to?',
     ('Rejected/Declined','Apply','Reviewed','Screen','Interview','Offer','Ready for Hire'))
 
-st.write(" ")
+st.markdown("<br>")
 
 option1 = st.multiselect(
     'What Business Units?',
     ['Finance','Marketing','HR','Operations','Product','Sales'])
 
-st.write(" ")
+st.markdown("<br>")
 
 st.write("Segmented Trending by ", job_step)
 
