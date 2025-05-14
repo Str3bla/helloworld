c3c1300dabbfb209cce64df85117e152fc5bd762
Str3bla <reportingology@gmail.com>
2025-05-14 18:51:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,10 +46,8 @@ st.write("")
 st.write("")
 st.write("")
 
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
-
-df = pd.read_csv("https://github.com/Str3bla/helloworld/blob/main/train.csv")
-pr = df.profile_report()
-
-st_profile_report(pr)
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
