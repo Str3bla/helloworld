8a382ea138e4f5465b11aa1afc98cac1d3a19d6f
Str3bla <reportingology@gmail.com>
2025-05-14 19:02:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,39 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+job_step = st.select_slider(
+    "Select a step in the applicant process to ANALYZE",
+    options=[
+        "Apply",
+        "Reviewed",
+        "Screen",
+        "Interview",
+        "Offer",
+        "Ready for Hire",
+    ],
+)
+
+st.sidebar.write("")
+
+option = st.selectbox(
+    'What do you want to compare it to?',
+    ('Rejected/Declined','Apply','Reviewed','Screen','Interview','Offer','Ready for Hire'))
+
+st.write("")
+
+option1 = st.multiselect(
+    'What Business Units?',
+    ['Finance','Marketing','HR','Operations','Product','Sales'])
+
+st.write("")
+
+st.write('What challenges do you want TA GPT to help with?')
+
+Scenario_TTF = st.checkbox("Time to Fill")
+Scenario_Difficulty = st.checkbox("Location")
+Scenario_Conversion = st.checkbox("Conversion")
+Scenario_Quantity = st.checkbox("Compensation")
+
 st.write("Segmented Trending by ", job_step)
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
@@ -35,39 +68,3 @@ secondaryBackgroundColor="#AED6F1"
 textColor="#FFFFFF"
 font="monospace"
 """)
-
-number = st.sidebar.slider('Select a number:', 0, 10, 5)
-st.sidebar.write('Selected number from slider widget is:', number)
-
-job_step = st.select_slider(
-    "Select a step in the applicant process to ANALYZE",
-    options=[
-        "Apply",
-        "Reviewed",
-        "Screen",
-        "Interview",
-        "Offer",
-        "Ready for Hire",
-    ],
-)
-
-st.sidebar.write("")
-
-option = st.selectbox(
-    'What do you want to compare it to?',
-    ('Rejected/Declined','Apply','Reviewed','Screen','Interview','Offer','Ready for Hire'))
-
-st.write("")
-
-option1 = st.multiselect(
-    'What Business Units?',
-    ['Finance','Marketing','HR','Operations','Product','Sales'])
-
-st.write("")
-
-st.write('What challenges do you want TA GPT to help with?')
-
-Scenario_TTF = st.checkbox("Time to Fill")
-Scenario_Difficulty = st.checkbox("Location")
-Scenario_Conversion = st.checkbox("Conversion")
-Scenario_Quantity = st.checkbox("Compensation")
