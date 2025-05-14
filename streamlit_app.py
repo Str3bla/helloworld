352e44e2f30ab836c2581c52b57c5cd1ef231d35
Str3bla <reportingology@gmail.com>
2025-05-14 18:46:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,9 +49,7 @@ st.write("")
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
-st.header('`streamlit_pandas_profiling`')
-
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-
+df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
 pr = df.profile_report()
+
 st_profile_report(pr)
