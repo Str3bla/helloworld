fcc318b0d38e106b548fe13c538712de1827c334
Str3bla <reportingology@gmail.com>
2025-05-14 18:48:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ st.write("")
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
-df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
+df = pd.read_csv("https://github.com/Str3bla/helloworld/blob/main/train.csv")
 pr = df.profile_report()
 
 st_profile_report(pr)
