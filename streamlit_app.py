472a15d8bd25fcbb672a7a5183cf4b718868d431
Str3bla <reportingology@gmail.com>
2025-05-19 14:09:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,11 +46,11 @@ st.write("")
 st.write("")
 st.write("")
 
-st.latex(r'''
-     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-     \sum_{k=0}^{n-1} ar^k =
-     a \left(\frac{1-r^{n}}{1-r}\right)
-     ''')
+# st.latex(r'''
+#     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+#     \sum_{k=0}^{n-1} ar^k =
+#     a \left(\frac{1-r^{n}}{1-r}\right)
+#     ''')
 
 st.write("")
 st.write("")
